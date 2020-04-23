3185af042001f6881dfb9e0356b87b5ec700fe67
dorista <hao_fei96@yahoo.ca>
2020-04-23 17:03:48 -0400

    Add TODO for next time

--- server.py
+++ server.py
@@ -13,12 +13,12 @@ class Battlesnake(object):
     @cherrypy.expose
     def index(self):
         # If you open your snake URL in a browser you should see this message.
-        return "Your Battlesnake is alive!"
+        return "Baby Blep licked you!"
 
     @cherrypy.expose
     def ping(self):
         # The Battlesnake engine calls this function to make sure your snake is working.
-        return "pong"
+        return
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -27,7 +27,10 @@ class Battlesnake(object):
         # This function is called everytime your snake is entered into a game.
         # cherrypy.request.json contains information about the game that's about to be played.
         data = cherrypy.request.json
-        print(data) # DEBUG
+        #gid = data["game"]["id"]
+        #health = data["you"]["health"]
+        #boardy = data["board"]["height"]
+        #boardx = data["board"]["width"]
         return {
             "color": "#FFFFFF",
             "headType": "bwc-rudolph",
@@ -41,7 +44,11 @@ class Battlesnake(object):
         # This function is called on every turn of a game. It's how your snake decides where to move.
         # Valid moves are "up", "down", "left", or "right".
         data = cherrypy.request.json
-        print(data) # DEBUG
+        #gid = data["game"]["id"]
+        #health = data["you"]["health"]
+        #boardy = data["board"]["height"]
+        #boardx = data["board"]["width"]
+
         # TODO: Compute a random direction to move in
         possible_moves = ["up", "down", "left", "right"]
         move = random.choice(possible_moves)
@@ -56,7 +63,8 @@ class Battlesnake(object):
         # It's purely for informational purposes, you don't have to make any decisions here.
         data = cherrypy.request.json
         print(data) # DEBUG
-        return "ok"
+        print("GG")
+        return
 
 
 if __name__ == "__main__":
